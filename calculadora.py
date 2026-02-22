f7b422cbfcfd959c449120f8a5da2f6aad3b5846
Andres <jonathanserranof18@gmail.com>
2026-02-21 23:33:26 -0500

    commit 2 de la funcion de suma terminado

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,9 @@
-print("prueba para saber mas info ---para el commit 1")
+print("calculadora")
+
+def suma():
+    num1 = float(input("ingresa el numero 1 : "))
+    num2 = float(input("ingresa el numero 2 : "))
+    print("la suma de los numero que ingresaste es  : ", num1+num2)
+
+
+suma()    
